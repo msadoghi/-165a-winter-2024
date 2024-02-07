ade5c35d0042581bd30627866803a561363bedee
zhao-18 <zhao1455@gmail.com>
2024-02-06 19:16:43 -0800

    Fix: changed column in RID class

--- lstore/RID.cpp
+++ lstore/RID.cpp
@@ -1,7 +1,7 @@
 #include "RID.h"
 
 bool RID::check_schema (int column_num) {
-    int schema_encoding = *(pointers[3]);
+    int schema_encoding = *(pointers[SCHEMA_ENCODING_COLUMN]);
     int bin = 0b1 & (schema_encoding >> (column_num - 1));
     return bin;
 }
--- lstore/RID.h
+++ lstore/RID.h
@@ -8,11 +8,10 @@
 class RID {
 public:
     // Schema of the record should be
-    // | Indirection | Rid | Timestamp | schema encoding | data | data | ... | data |
+    // | Indirection | Timestamp | schema encoding | data | data | ... | data |
     const int INDIRECTION_COLUMN = 0;
-    const int RID_COLUMN = 1;
-    const int TIMESTAMP_COLUMN = 2;
-    const int SCHEMA_ENCODING_COLUMN = 3;
+    const int TIMESTAMP_COLUMN = 1;
+    const int SCHEMA_ENCODING_COLUMN = 2;
     RID ();
     virtual ~RID ();
     std::vector<int*> pointers;
