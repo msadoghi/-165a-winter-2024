a1eb42fb644ed793ae8fe51db89e0f79a728cc8b
Trevorwl <tlash1995@gmail.com>
2024-02-06 17:15:43 -0800

    Main tester

---
+++ __main__.cpp
@@ -0,0 +1,116 @@
+#include <chrono>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <ratio>
+#include <vector>
+
+
+#include "lstore/db.h"
+#include "query.h"
+#include "table.h"
+#include "__main__.h"
+
+Database db;
+
+std::vector<int> keys;
+
+Table grades_table = db.create_table("Grades",5,0);
+
+Query query = Query(grades_table);
+
+std::vector<int>keys;
+
+void testInsert(){
+	auto startTime = std::chrono::high_resolution_clock::now();
+
+	for(int i = 0;i < 10000;i++){
+		query.insert(std::vector{906659671 + i, 93, 0, 0, 0});
+
+		keys.push_back(906659671 + i);
+	}
+
+	auto endTime = std::chrono::high_resolution_clock::now();
+
+	printf("Inserting 10k records took: %.2f ms\n\n",
+			std::chrono::duration<double, std::milli>{endTime-startTime});
+}
+
+void testUpdate(){
+	srand(time(0));
+
+	std::vector<std::vector<int>> update_cols{
+		std::vector<int>{0,0,0,0,0},
+		std::vector<int>{0,rand()%100,0,0,0},
+		std::vector<int>{0,0,rand()%100,0,0},
+		std::vector<int>{0,0,0,rand()%100,0},
+		std::vector<int>{0,0,0,0,rand()%100},
+	};
+
+	auto startTime = std::chrono::high_resolution_clock::now();
+
+	for(int i = 0;i<10000;i++){
+		query.update(keys[rand()%keys.size()],
+				update_cols[rand()%keys.size()]);
+	}
+
+	auto endTime = std::chrono::high_resolution_clock::now();
+
+	printf("Updating 10k records took: %.2f ms\n\n",
+			std::chrono::duration<double, std::milli>{endTime-startTime});
+
+}
+
+std::vector<int>projectedColumns{1,1,1,1,1};
+
+void testSelect(){
+	auto startTime = std::chrono::high_resolution_clock::now();
+	for(int i = 0;i<10000;i++){
+		query.select(keys[rand()%keys.size()], 0, projectedColumns);
+	}
+
+	auto endTime = std::chrono::high_resolution_clock::now();
+
+	printf("Selecting 10k records took: %.2f ms\n\n",
+					std::chrono::duration<double, std::milli>{endTime-startTime});
+}
+
+void testAggregation(){
+	auto startTime = std::chrono::high_resolution_clock::now();
+
+	for(int i = 0;i < 10000;i += 100){
+		int start_value = 906659671 + i;
+	    int end_value = start_value + 100;
+		int result = query.sum(start_value, end_value - 1, rand() % 5);
+	}
+
+	auto endTime = std::chrono::high_resolution_clock::now();
+	printf("Aggregate 10k of 100 record batch took %.2f ms\n\n", endTime - startTime);
+
+}
+
+void testDelete(){
+	auto startTime = std::chrono::high_resolution_clock::now();
+
+	for(int i=0;i<10000;i++){
+		/*
+		 * Delete method is probably renamed, needs to be checked
+		 */
+		query.Delete(906659671 + i);
+	}
+
+	auto endTime = std::chrono::high_resolution_clock::now();
+
+	printf("Deleting 10k records took:  %.2f ms\n\n", endTime - startTime);
+}
+
+void mainTest(){
+	testInsert();
+	testUpdate();
+	testSelect();
+	testAggregation();
+	testDelete();
+}
+
+
+
