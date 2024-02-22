5a2c2a67590e8c7a02a9c16a3073cbfd9f1874d7
Lucas Nguyen <lucasnguyen@ucdavis.edu>
2024-02-22 14:43:14 -0800

    bufferpool

---
+++ lstore/bufferpool.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+from lstore.page import Page, PageRange
+from lstore.config import *
+import numpy as np
+import pickle
+import os
+
+class BufferPool:
+    path = ""
+    pool = {}
+    capacity = 0
+    
+    def __init__(cls, capacity = 2000):
+        cls.capcity = capacity
+        pass
+        
+    @classmethod 
+    def initial_path(cls, path):
+        cls.path = path
+    
+    @classmethod 
+    def page_buffer_checker(cls, buffer_id):
+        return buffer_id in cls.pool.keys()
+           
+    @classmethod  
+    def add_pages(cls, buffer_id, page):
+        cls.pool[buffer_id] = page
+        cls.pool[buffer_id].set_dirty()
+        
+    @classmethod 
+    def updata_pool(cls, buffer_id, page):
+        cls.pool[buffer_id] = page
+        cls.pool[buffer_id].set_dirty()
+
+    @classmethod 
+    def is_full(cls):
+        return cls.LRU.is_full()
+
+    @classmethod 
+    def bufferid_path_pkl(cls, buffer_id):
+        dirname = os.path.join(cls.path, str(buffer_id[2]), str(buffer_id[3]), buffer_id[1])
+        file_path = os.path.join(dirname, str(buffer_id[4]) + '.pkl')
+        return file_path
+    
+
+    @classmethod 
+    def get_page(cls, buffer_id):
+        if buffer_id in cls.pool:
+            return cls.pool[buffer_id]
+        path = cls.bufferid_path_pkl(buffer_id)
+        # create new_page
+        if not os.path.isfile(path):
+            page = Page()
+            cls.add_pages(buffer_id, page)
+            return page
+        # page already exist
+        else:
+            if not cls.page_buffer_checker(buffer_id):
+                cls.pool[buffer_id] = cls.read_page(path)
+            return cls.pool[buffer_id]
+        
+    @classmethod 
+    def read_page(cls, path):
+        f = open(path, 'r+b')
+        page = Page()
+        metadata = pickle.load(f)
+        
+        page.num_records = metadata[0]
+        page.dirty = metadata[1]
+        page.pinned = metadata[2]
+        page.tps = metadata[3]
+        page.data = pickle.load(f)
+        f.close()
+        return page
+       
+    @classmethod  
+    def write_page(cls, page, buffer_id):
+        dirname = os.path.join(cls.path, str(buffer_id[2]), str(buffer_id[3]), buffer_id[1])
+        file_path = os.path.join(dirname, str(buffer_id[4]) + '.pkl')
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        f = open(file_path, 'w+b')
+        metadata = [page.num_records, page.dirty, page.pinned, page.tps]
+        pickle.dump(metadata, f)
+        pickle.dump(page.data, f)
+        f.close()
+
+    @classmethod 
+    def close(cls):
+        for buffer_id in cls.pool:
+            cls.write_page(cls.pool[buffer_id], buffer_id)
+
--- lstore/table.py
+++ lstore/table.py
@@ -1,6 +1,7 @@
 from lstore.index import Index
 from lstore.page import Page
 from lstore.config import *
+from lstore.bufferpool import BufferPool
 from time import time
 
 # One record has a Page per column
@@ -95,3 +96,134 @@ class Table:
         print("merge is happening")
         pass
  
+class Table:
+    """
+    :param name: string         #Table name
+    :param num_columns: int     #Number of Columns: all columns are integer
+    :param key: int             #Index of table key in columns
+    """
+
+    def __init__(self, name, num_columns, key_column):
+        self.table_path = ""
+        self.name = name
+        self.key_column = key_column
+        self.num_columns = num_columns
+        self.total_num_columns = self.num_columns+(META_COLUMN_COUNT + 1)
+        self.page_directory = {}
+        self.index = Index(self)
+        self.num_records = 0
+        self.num_updates = 0
+        self.key_RID = {}
+        
+        '''
+        # Key: (table_name, col_index, page_range_index), value: threading lock 
+        self.rid_locks = defaultdict(lambda: defaultdict(threading.Lock())) 
+        # Key: (table_name, col_index, page_range_index,page_index), value: threading lock  
+        self.page_locks = defaultdict(lambda: defaultdict(threading.Lock()))
+        '''
+        
+    def get_rid(self, key):
+        return self.key_RID[key]
+
+    # using a number to find the location of the page 
+    def determine_page_location(self, type):
+        num_tail = self.num_updates
+        num_base = self.num_records - num_tail
+        page_range_index = num_base % RECORDS_PER_PAGE_RANGE
+        if type == 'base':
+            base_page_index = num_base % RECORDS_PER_PAGE
+            return page_range_index, base_page_index
+        else:
+            tail_page_index = num_tail % RECORDS_PER_PAGE
+            return page_range_index, tail_page_index
+
+    # column is the insert data
+    def base_write(self, columns):
+        # self.new_record.acquire()
+        page_range_index, base_page_index = self.determine_page_location('base')
+        for i, value in enumerate(columns):     
+            # use buffer_id to find the page
+            buffer_id = (self.name, "base", i, page_range_index, base_page_index)
+            page = BufferPool.get_page(buffer_id)
+            
+            if not page.has_capacity():
+                if base_page_index == BASE_PAGE_MAX - 1:
+                    page_range_index += 1
+                    base_page_index = 0
+                else:
+                    base_page_index += 1
+                buffer_id = (self.name, "base", i, page_range_index, base_page_index)
+            
+            
+            # write in
+            page.write(value)
+            offset = page.num_records - 1
+            BufferPool.updata_pool(buffer_id, page)  
+        
+        # write address into page directory
+        rid = columns[0]
+        address = [self.name, "base", page_range_index, base_page_index, offset]
+        self.page_directory[rid] = address
+        self.key_RID[columns[self.key_column + (META_COLUMN_COUNT + 1)]] = rid
+        self.num_records += 1
+        self.index.push_index(columns[(META_COLUMN_COUNT + 1):len(columns) + 1], rid)
+        # self.new_record.release()
+        
+
+    def tail_write(self, columns):
+        # self.update_record.acquire()
+        page_range_index, tail_page_index = self.determine_page_location('tail')
+        # print("column in baseWrite: {}".format(column))
+        for i, value in enumerate(columns):
+            # use buffer_id to find the page
+            buffer_id = (self.name, "tail", i, page_range_index,tail_page_index)
+            page = BufferPool.get_page(buffer_id)
+            
+            if not page.has_capacity():
+                tail_page_index += 1
+                buffer_id = (self.name, "tail", i, page_range_index,tail_page_index)
+                    
+            page = BufferPool.get_page(buffer_id)
+            # write in
+            # print("value in tail_write: {} {}".format(i, value))
+            page.write(value)
+            offset = page.num_records - 1
+            BufferPool.updata_pool(buffer_id, page)
+            
+        # write address into page directory
+        rid = columns[0]
+        address = [self.name, "tail", page_range_index, tail_page_index, offset]
+        self.page_directory[rid] = address
+        self.key_RID[columns[self.key_column + (META_COLUMN_COUNT + 1)]] = rid
+        self.num_records += 1
+        self.num_updates += 1
+        # self.update_record.release()
+        
+    def find_value_rid(self, column_index, target):
+        rids = []
+        for rid in self.page_directory:
+            record = self.find_record(rid)
+            if record[column_index + (META_COLUMN_COUNT + 1)] == target:
+                rids.append(rid)
+        return rids
+
+    # pages is the given column that we are going to find the sid
+    def find_value(self, column_index, location):
+        buffer_id = (location[0], location[1], column_index, location[2], location[3])
+        page = BufferPool.get_page(buffer_id)
+        value = page.get_value(location[4])
+        return value
+
+    def update_value(self, column_index, location, value):
+        buffer_id = (location[0], location[1], column_index, location[2], location[3])
+        page = BufferPool.get_page(buffer_id)
+        page.update(location[4], value)
+        BufferPool.updata_pool(buffer_id, page)
+
+    def find_record(self, rid):
+        row = []
+        location = self.page_directory[rid]
+        for i in range((META_COLUMN_COUNT + 1) + self.num_columns):
+            result = self.find_value(i, location)
+            row.append(result)
+        return row
